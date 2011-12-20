fcea8eec5906baa273bcf56ade45c749de19f937
Ben Roberts <ben@sihnon.net>
2011-12-20 01:43:35 +0000

    Make character escaping consistent

--- source/webui/pages/errors/404.php
+++ source/webui/pages/errors/404.php
@@ -3,7 +3,7 @@
 $main = StatusBoard_Main::instance();
 $req  = $main->request();
 
-$this->smarty->assign('requested_page', htmlspecialchars($req->request_string()));
+$this->smarty->assign('requested_page', $req->request_string());
 
 
 ?>
