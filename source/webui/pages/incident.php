1edfb5e2c8d38614746f7d07b35925bb09a336c7
Ben Roberts <ben@sihnon.net>
2012-01-17 00:20:07 +0000

    Fix indentation

--- source/webui/pages/incident.php
+++ source/webui/pages/incident.php
@@ -1,29 +1,24 @@
-
 <?php
+
 $main = StatusBoard_Main::instance();
 $request = $main->request();
 $auth = $main->auth();
 
-$incident_id = $request->get('id', 'Sihnon_Exception_InvalidParameters');
-
-try 
-	{
+try {
+    $incident_id = $request->get('id', 'Sihnon_Exception_InvalidParameters');
     $incident = StatusBoard_Incident::fromId($incident_id);
+    
     $site_id = $incident->site;
     $site = StatusBoard_Site::fromId($site_id);
+    
     $service_id = $site->service;
     $service = StatusBoard_Service::fromId($service_id);
-	}
-catch (Sihnon_Exception_ResultCountMismatch $e) 
-	{
+    
+} catch (Sihnon_Exception_ResultCountMismatch $e) {
     throw new StatusBoard_Exception_FileNotFound();
-
-	}
-
-
+}
 
 $statuses = $incident->statusChanges();
-
 
 $this->smarty->assign('service', $service);
 $this->smarty->assign('site', $site);
@@ -33,7 +28,5 @@ $this->smarty->assign('messages', $messages);
 
 $display_admin_links = ($auth->isAuthenticated() && $auth->isAdministrator());
 $this->smarty->assign('display_admin_links', $display_admin_links);
-
-
 
 ?>
