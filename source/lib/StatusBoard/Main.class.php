d0e85ab12313e4c699d30b7a2792ab4a476dfe48
Nathan Booth <nath.booth@gmail.com>
2012-06-05 21:21:09 +0100

    Version set to 2.0.0 ready for merge to master
    Signed-off-by: Nathan Booth <nath.booth@gmail.com>

--- source/lib/StatusBoard/Main.class.php
+++ source/lib/StatusBoard/Main.class.php
@@ -38,7 +38,7 @@ class StatusBoard_Main extends SihnonFramework_Main {
                 $this->smarty->registerPlugin('modifier', 'formatFilesize', array('StatusBoard_Main', 'formatFilesize'));
                 $this->smarty->registerPlugin('modifier', 'fuzzyTime', array('StatusBoard_DateTime', 'fuzzyTime'));
                 
-                $this->smarty->assign('version', '2.0.0_rc1');
+                $this->smarty->assign('version', '2.0.0');
                 $this->smarty->assign('version_codename', 'Bilberry');
                 $this->smarty->assign('messages', array());
                  
